a1b139f5019f32cd462026926504562b6dfe7d27
Stone <samstone9001@gmail.com>
2022-07-04 13:52:20 -0400

    closer to improving karma

--- src/main/java/reactorRecorders/KarmaCounter.java
+++ src/main/java/reactorRecorders/KarmaCounter.java
@@ -2,9 +2,11 @@ package reactorRecorders;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -23,6 +25,7 @@ public class KarmaCounter {
 	private Map<Long, MutableInteger> karmaCounts;
 	private Map<Long, MutableInteger> givenCounts;
 	private static Pattern mentionPattern = Pattern.compile("(?:<@!?)(\\d*?)(?:>) *?([+-]{2}|[+-]= *(-?\\d+))");
+	private static Pattern shortcutPattern = Pattern.compile("^((?:(?:\\+|-)=)|(?:\\+\\+|--)) *((?:-|\\+)?\\d+)?");
 	// private static int MENTION_OFFSET = 3;
 	private boolean shouldSave;
 	private File karmaFile;
@@ -63,10 +66,10 @@ public class KarmaCounter {
 		this.shouldSave = shouldAffect;
 	}
 
-	public List<Long> findKarma(String content, Long author) {
+	public Set<Long> findKarma(String content, Long author) {
 
 		Matcher matcher = mentionPattern.matcher(content);
-		List<Long> receivers = new ArrayList<>();
+		Set<Long> receivers = new HashSet<>();
 		if (author == 456226577798135808l)
 		{ author = 827724526313537536l; }
 
@@ -96,29 +99,29 @@ public class KarmaCounter {
 	}
 
 	public void accept(Message message) {
-		List<Long> receivingUsers = findKarma(message.getContentRaw(), message.getAuthor().getIdLong());
+		Set<Long> receivingUsers = findKarma(message.getContentRaw(), message.getAuthor().getIdLong());
 
 		
 		//doing it like this insures that we only make a call to discord only if the message is actually trying to add or subtract karma
-			if (message.getContentRaw().startsWith("++"))
+		Matcher shortcutFinder = shortcutPattern.matcher(message.getContentRaw());
+		if (shortcutFinder.find())
+		{
+			String amount = shortcutFinder.group(2);
+			String decider = shortcutFinder.group(1);
+			Action action = Action.actionOf(decider);
+			int karmaAmount;
+			if (amount != null)
 			{
-				Message referenced = message.getReferencedMessage();
-				long receiver = referenced.getAuthor().getIdLong();
-				if (referenced != null)
-				{
-					receivingUsers.add(receiver);
-					giveKarma(receiver, message.getAuthor().getIdLong(), 1);
-				}
-			} else if (message.getContentRaw().startsWith("--")) {
-				Message referenced = message.getReferencedMessage();
-				long receiver = referenced.getAuthor().getIdLong();
-				
-				if (referenced != null)
-				{
-				receivingUsers.add(receiver);
-				giveKarma(receiver, message.getAuthor().getIdLong(), -1);
-				}
+				karmaAmount = Integer.valueOf(amount);
+			} else
+			{
+				karmaAmount = 0; //doesn't matter
 			}
+			long receiver = message.getReferencedMessage().getAuthor().getIdLong();
+			long giver = message.getAuthor().getIdLong();
+			Karma karma = new Karma(giver, receiver, action, karmaAmount);
+			receivingUsers.add(receiver);
+		}
 
 		if (!receivingUsers.isEmpty())
 			if (shouldSave)
@@ -126,12 +129,8 @@ public class KarmaCounter {
 				{
 					String output = "";
 					Guild guild = message.getGuild();
-					Long lastUser = null;
 					for (Long id : receivingUsers)
 					{
-						if (id.equals(lastUser))
-							continue;
-						lastUser = id;
 						Member user = guild.retrieveMemberById(id).complete();
 						output += user.getEffectiveName() + " now has " + getKarma(id) + " karma, ";
 					}
@@ -215,10 +214,62 @@ public class KarmaCounter {
 			output += "\n";
 		}
 		return output;
-
+		
 	}
 
 	public KarmaCounter copyOf(boolean shouldResetCounts, boolean shouldAffect) {
 		return new KarmaCounter(karmaCounts, givenCounts, karmaFile, givenFile, shouldResetCounts, shouldAffect);
+	}
+	
+	private class Karma {
+		
+		public long giver;
+		public long receiver;
+		
+		public int amount;
+		
+		public Karma(long giver, long receiver, Action action, int amount) {
+			this.giver = giver;
+			this.receiver = receiver;
+			switch (action)
+			{
+			case INCREMENT:
+				this.amount = 1;
+				break;
+			case ADD:
+				this.amount = amount;
+				break;
+			case DECREMENT:
+				this.amount = -1;
+				break;
+			case SUBTRACT:
+				this.amount = -amount;
+				break;
+			default:
+				this.amount = 0;
+				break;
+			}
+		}
+		
+	}
+	
+	protected enum Action {
+		ADD, SUBTRACT, INCREMENT, DECREMENT;
+		
+		static Action actionOf(String str) {
+			switch (str)
+			{
+			case "++":
+				return INCREMENT;
+			case "+=":
+				return ADD;
+			case "--":
+				return DECREMENT;
+			case "-=":
+				return SUBTRACT;
+			default:
+				return null;
+			}
+		}
 	}
 }
