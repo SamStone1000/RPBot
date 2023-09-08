27716290af25d8624b67912ce20645af5672b9ee
Stone <samstone9001@gmail.com>
2023-09-08 16:27:02 -0400

    It works!

--- src/main/java/stone/rpbot/slash/TimeCommand.java
+++ src/main/java/stone/rpbot/slash/TimeCommand.java
@@ -1,54 +1,68 @@
 package stone.rpbot.slash;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoField;
+import java.time.ZoneId;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.utils.TimeFormat;
 
 public class TimeCommand implements SlashCommand {
 
-	private static final TimeUnit[] CLOCK_UNITS = { TimeUnit.HOURS, TimeUnit.MINUTES, TimeUnit.SECONDS };
+    private static final TimeUnit[] CLOCK_UNITS = { TimeUnit.HOURS, TimeUnit.MINUTES, TimeUnit.SECONDS };
+    private static final Map<Character, ChronoField> SIMPLE_UNITS = new HashMap<>();
 
-	@Override
-	public void onSlashCommand(SlashCommandInteractionEvent event) {
-		System.out.println("aaaaaaaaa");
-		Modifiers mod = Modifiers.STATIC;
-		String input = event.getOption("input").getAsString();
-
-		if (input.startsWith("+"))
-			mod = Modifiers.ADDITIVE;
-		else if (input.startsWith("-"))
-			mod = Modifiers.SUBTRACTIVE;
-		String[] tokens = input.split(":");
-		if (tokens.length > 3) {
-			event.reply("stupid").queue();
-			return;
-		}
-
-		long amount = 0;
-		int i = 3 - tokens.length;
-		try {
-			for (String token : tokens) {// HH:MM:SS format
-				int tokenTime = Integer.parseInt(token);
-				amount += CLOCK_UNITS[i].toMillis(tokenTime);
-			}
-		} catch (NumberFormatException exception) {
-			event.reply(exception.getMessage()).setEphemeral(true).queue();
-			return;
-		}
-		if (mod == Modifiers.SUBTRACTIVE)
-			amount *= -1;
-		event.reply(TimeFormat.DEFAULT.format(System.currentTimeMillis() + amount)).setEphemeral(true).queue();
+    static {
+	SIMPLE_UNITS.put('h', ChronoField.HOUR_OF_DAY);
+	SIMPLE_UNITS.put('m', ChronoField.MINUTE_OF_HOUR);
+	SIMPLE_UNITS.put('s', ChronoField.SECOND_OF_MINUTE);
+	SIMPLE_UNITS.put('d', ChronoField.DAY_OF_MONTH);
+	//SIMPLE_UNITS.put('m', ChronoField.MONTH_OF_YEAR);
+	SIMPLE_UNITS.put('y', ChronoField.YEAR);
+    }
+    
+    @Override
+    public void onSlashCommand(SlashCommandInteractionEvent event) {
+	String input = event.getOption("input").getAsString();
+	OffsetDateTime dateTime = OffsetDateTime.now(ZoneId.of("America/Louisville"));
+	for (String token : input.split(" ")) {
+	    if(token.contains(":")) {
+		//handleClock(dateTime, token);
+	    } else {
+		dateTime = handleSimpleUnit(dateTime, token);
+	    }
 	}
+	event.reply(TimeFormat.DEFAULT.format(dateTime) + " " + dateTime).setEphemeral(true).queue();
+    }
 
-	public enum Modifiers {
-		ADDITIVE, STATIC, SUBTRACTIVE;
+    public OffsetDateTime handleSimpleUnit(OffsetDateTime dateTime, String token) {
+	Character unit = token.charAt(token.length() - 1);
+	int amount = Integer.valueOf(token.substring(0, token.length() - 1));
 
-	}
+	System.out.println(unit + " " + amount);
+	ChronoField timeField = SIMPLE_UNITS.get(unit);
 
-	public static void main(String[] args) {
-		String[] tokens = "10".split(":");
-		System.out.println(Arrays.toString(tokens));
-	}
+	if (token.charAt(0) == '+' || token.charAt(0) == '-') //means this token
+	    {					              //is a relative amount
+		int timeAmount = dateTime.get(timeField);
+		return dateTime.with(timeField, timeAmount + amount);
+	    } else //means this token is absolute and is overwriting a field
+	    {
+		return dateTime.with(timeField, amount);
+	    }
+    }
+		    
+    public enum Modifiers {
+	RELATIVE, ABSOLUTE;
+
+    }
+
+    public static void main(String[] args) {
+	String[] tokens = "10".split(":");
+	System.out.println(Arrays.toString(tokens));
+    }
 }
