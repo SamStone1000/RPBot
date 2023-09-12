f477c84c35986f88af2cd66af6f3d18d2342500f
stone <samstone9001@gmail.com>
2023-09-11 22:30:24 -0400

    Add field wrapping + months

--- src/main/java/stone/rpbot/slash/TimeCommand.java
+++ src/main/java/stone/rpbot/slash/TimeCommand.java
@@ -1,68 +1,61 @@
 package stone.rpbot.slash;
 
-import java.util.Arrays;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoField;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.time.OffsetDateTime;
-import java.time.temporal.ChronoField;
-import java.time.ZoneId;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.utils.TimeFormat;
 
 public class TimeCommand implements SlashCommand {
 
-    private static final TimeUnit[] CLOCK_UNITS = { TimeUnit.HOURS, TimeUnit.MINUTES, TimeUnit.SECONDS };
-    private static final Map<Character, ChronoField> SIMPLE_UNITS = new HashMap<>();
+	private static final TimeUnit[] CLOCK_UNITS = { TimeUnit.HOURS, TimeUnit.MINUTES, TimeUnit.SECONDS };
+	private static final Map<Character, ChronoField> SIMPLE_FIELDS = new HashMap<>();
 
-    static {
-	SIMPLE_UNITS.put('h', ChronoField.HOUR_OF_DAY);
-	SIMPLE_UNITS.put('m', ChronoField.MINUTE_OF_HOUR);
-	SIMPLE_UNITS.put('s', ChronoField.SECOND_OF_MINUTE);
-	SIMPLE_UNITS.put('d', ChronoField.DAY_OF_MONTH);
-	//SIMPLE_UNITS.put('m', ChronoField.MONTH_OF_YEAR);
-	SIMPLE_UNITS.put('y', ChronoField.YEAR);
-    }
-    
-    @Override
-    public void onSlashCommand(SlashCommandInteractionEvent event) {
-	String input = event.getOption("input").getAsString();
-	OffsetDateTime dateTime = OffsetDateTime.now(ZoneId.of("America/Louisville"));
-	for (String token : input.split(" ")) {
-	    if(token.contains(":")) {
-		//handleClock(dateTime, token);
-	    } else {
-		dateTime = handleSimpleUnit(dateTime, token);
-	    }
+	static {
+		SIMPLE_FIELDS.put('h', ChronoField.HOUR_OF_DAY);
+		SIMPLE_FIELDS.put('m', ChronoField.MINUTE_OF_HOUR);
+		SIMPLE_FIELDS.put('s', ChronoField.SECOND_OF_MINUTE);
+		SIMPLE_FIELDS.put('d', ChronoField.DAY_OF_MONTH);
+		SIMPLE_FIELDS.put('D', ChronoField.DAY_OF_MONTH);
+		SIMPLE_FIELDS.put('M', ChronoField.MONTH_OF_YEAR);
+		SIMPLE_FIELDS.put('y', ChronoField.YEAR);
+		SIMPLE_FIELDS.put('Y', ChronoField.YEAR);
 	}
-	event.reply(String.format("`%s`", TimeFormat.RELATIVE.format(dateTime))).setEphemeral(true).queue();
-    }
 
-    public OffsetDateTime handleSimpleUnit(OffsetDateTime dateTime, String token) {
-	Character unit = token.charAt(token.length() - 1);
-	int amount = Integer.valueOf(token.substring(0, token.length() - 1));
+	@Override
+	public void onSlashCommand(SlashCommandInteractionEvent event) {
+		String input = event.getOption("input").getAsString();
+		OffsetDateTime dateTime = OffsetDateTime.now(ZoneId.of("America/Louisville"));
+		for (String token : input.split(" ")) {
+			if (token.contains(":")) {
+				// handleClock(dateTime, token);
+			} else {
+				dateTime = handleSimpleUnit(dateTime, token);
+			}
+		}
+		event.reply(String.format("`%s`", TimeFormat.RELATIVE.format(dateTime))).setEphemeral(true).queue();
+	}
 
-	System.out.println(unit + " " + amount);
-	ChronoField timeField = SIMPLE_UNITS.get(unit);
+	public OffsetDateTime handleSimpleUnit(OffsetDateTime dateTime, String token) {
+		Character unit = token.charAt(token.length() - 1);
+		int amount = Integer.valueOf(token.substring(0, token.length() - 1));
+		ChronoField timeField = SIMPLE_FIELDS.get(unit);
 
-	if (token.charAt(0) == '+' || token.charAt(0) == '-') //means this token
-	    {					              //is a relative amount
-		int timeAmount = dateTime.get(timeField);
-		return dateTime.with(timeField, timeAmount + amount);
-	    } else //means this token is absolute and is overwriting a field
-	    {
-		return dateTime.with(timeField, amount);
-	    }
-    }
-		    
-    public enum Modifiers {
-	RELATIVE, ABSOLUTE;
+		if (token.charAt(0) == '+' || token.charAt(0) == '-') // means this token
+		{ // is a relative amount
+			return dateTime.plus(amount, timeField.getBaseUnit());
+		} else // means this token is absolute and is overwriting a field
+		{
+			return dateTime.with(timeField, amount);
+		}
+	}
 
-    }
+	public enum Modifiers {
+		RELATIVE, ABSOLUTE;
 
-    public static void main(String[] args) {
-	String[] tokens = "10".split(":");
-	System.out.println(Arrays.toString(tokens));
-    }
+	}
 }
