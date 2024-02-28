d5fced9898e9a5834853f1fed0a454f71dfb21e5
Stone <samstone9001@gmail.com>
2024-02-28 14:50:45 -0500

    sync

---
+++ src/main/java/stone/rpbot/slash/commands/CommandTag.java
@@ -0,0 +1,12 @@
+package stone.rpbot.slash.commands;
+
+import stone.rpbot.slash.SlashCommand;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class CommandTag implements SlashCommand {
+
+    @Override
+    public void onSlashCommand(SlashCommandInteractionEvent event) {
+        
+    }
