14cb1aa31cb568ea65b8b61905694294a1d70014
Stone <samstone9001@gmail.com>
2024-02-29 17:12:46 -0500

    sync

--- src/main/java/stone/rpbot/slash/commands/tag/AbstractTag.java
+++ src/main/java/stone/rpbot/slash/commands/tag/AbstractTag.java
@@ -1,7 +1,20 @@
 package stone.rpbot.slash.commands.tag;
 
-public abstract class AbstractTag implements Tag {
+import java.util.concurrent.atomic.AtomicLong;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public abstract class AbstractTag implements Tag {
     
+    private static final AtomicLong nextID;
+    private static final Connection DATABASE;
+
+    private static final Path IDFile = Path.of(SharedConstants.BIN + "tag", "id.txt");
+    
+    private final long ID;
+
+    static {
+        DATABASE = DriverManager.getConnection("jdbc:derby:tags;create=true");
+        
     
     private void readData(
