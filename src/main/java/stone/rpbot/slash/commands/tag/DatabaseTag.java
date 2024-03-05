a0929c04d0ffca86c8a2707aafd065ee7dd8782b
stone <samstone9001@gmail.com>
2024-03-04 22:40:02 -0500

    sync

--- src/main/java/stone/rpbot/slash/commands/tag/DatabaseTag.java
+++ src/main/java/stone/rpbot/slash/commands/tag/DatabaseTag.java
@@ -22,7 +22,7 @@ public class DatabaseTag implements Tag {
         this.id = id;
     }
 
-    public DatabaseTag(String name, String description, String table, Set<String> aliases, Tag superTag, Set<Tag> subTags) {
+    public DatabaseTag(String name, String description, String table, String nameID, Set<String> aliases, Tag superTag, Set<Tag> subTags) {
         try (Statement statement = DATABASE.createStatement()) {
             if (statement.execute("SELECT normalized FROM _tables WHERE name = "+table+';')) {
                 try (ResultSet rs = state.getResultSet()) {
@@ -32,6 +32,18 @@ public class DatabaseTag implements Tag {
             } else {// Nothing found
                 throw new TagDatabaseException(TagDatabaseException.Type.NO_TABLE, "The specified table: "+table+" doesn't exist!");
             }
+
+            if (!isValidNameID(nameID)) {
+                throw new TagDatabaseException(TagDatabaseException.Type.INVALID_NAME_TAG, "The name ID was invalid!");
+            }
+
+            this.nameID = normalizeNameID(nameID);
+
+            if (statement.execute("SELECT * FROM "+this.table+" WHERE name = "+this.nameID+';')) {
+                throw new TagDatabaseException(TagDatabaseException.Type.TAG_EXISTS, "The tag already exists");
+            }
+
+            
         }
     }
     
@@ -40,9 +52,22 @@ public class DatabaseTag implements Tag {
         selectInfo.setLong(1, id);
     }
 
+    private String normalizeNameID(String nameID) {
+        try (Statement statement = DATABASE.createStatement()) {
+            
+
+    private boolean isValidNameID(String nameID) {
+        for (int i=0; i<str.length(); i++) {
+            char c = str.charAt(i);
+            if (c < 0x30 || (c >= 0x3a && c <= 0x40) || (c > 0x5a && c <= 0x60) || c > 0x7a)
+                return false;
+        }
+        return true;
+    }
+
     public static class TagDatabaseException extends Exception {
         public static enum Type {
-            NO_TABLE, TAG_EXISTS
+            NO_TABLE, TAG_EXISTS, INVALID_NAME_ID
         }
 
         Type type;
---
+++ src/main/java/stone/rpbot/slash/commands/tag/DatabaseTagBuilder.java
@@ -0,0 +1,10 @@
+package stone.rpbot.slash.commands.tag;
+
+public class DatabaseTagBuilder {
+
+    public DatabaseTagBuilder() {};
+
+    public Tag build();
+
+    public DatabaseTagBuilder setID(String ID) {
+        
